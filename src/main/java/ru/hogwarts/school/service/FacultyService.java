21fa66cac737d200cd569cf2630440be3fb00370
PoshiloySmih <mrforxbox@mail.ru>
2024-10-07 16:53:39 +0300

    Создание модели и сервисов веб-приложения

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FacultyService {
+    Map<Long, Faculty> faculties = new HashMap<>();
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentService {
+    Map<Long, Student> students = new HashMap<>();
+    private Long studentId = 1L;
+
+
+}
